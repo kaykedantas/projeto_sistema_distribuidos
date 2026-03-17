45f142a671f9bbecb1e0d54542cca8ea483d536f
kaykedantas <kaykedantas@gmail.com>
2026-03-16 21:28:17 -0300

    atualizacao_Worker

---
+++ aula16_03/worker.py
@@ -0,0 +1,21 @@
+import socket 
+import json 
+
+HOST = '10.62.206.46'
+PORT = 8000
+
+J= {
+"SERVER_UUID": "...",
+"TASK": "HEARTBEAT"
+}
+MENSAGEM = f" ola {HOST} /n {json.dumps(J)}"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.sendall(MENSAGEM.encode())
+print('Mensagem enviada com sucesso')
+print(json.dumps(J))
+
+teste = s.recv(1024)
+teste = teste.decode()
+print (teste)
--- client.py
+++ client.py
@@ -1,10 +1,11 @@
 
 import socket 
 import time 
-HOST = '10.62.206.45'
+HOST = '10.62.206.46'
 PORT = 8000
 ALUNO = 'Kayke Andrade Dantas '
 MENSAGEM = f'Olá: {HOST} \n {ALUNO}'
+MENSAGEM_2 = f'Olá: {HOST} \n {ALUNO} \n Mensagem 2'
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
@@ -17,8 +18,7 @@ teste = teste.decode()
 print(teste)
 if teste == "NOK":
     print("Mensagem não foi enviada")
-    s.sendall(MENSAGEM.encode())
+s.sendall(MENSAGEM_2.encode())
 s.close()
     
 #time.sleep(5)
-
